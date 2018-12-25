aae372f9ad36f086e00704e5306fb6304a8aed7c
JoonJang95 <joon.jang95@gmail.com>
2018-12-25 16:29:18 -0500

    Add initial CSS styling

--- client/components/Accessories.jsx
+++ client/components/Accessories.jsx
@@ -8,12 +8,36 @@ class Accessories extends React.Component {
   render() {
     return (
       <div id="accessories">
-        <div> Consider these accessories </div>
-        <span> image1 </span>
-        <span> image2 </span>
-        <span> image3 </span>
-        <span> image4 </span>
-        <span> image5 </span>
+        <div>
+          <div id="accessoryHead"> Consider these accessories </div>
+          <ul id="accessoryList">
+            <li>
+              <div>
+                <img src="https://target.scene7.com/is/image/Target/50220026?wid=250&hei=250&qlt=80&fmt=pjpeg" />
+              </div>
+            </li>
+            <li>
+              <div>
+                <img src="https://target.scene7.com/is/image/Target/50512957?wid=250&hei=250&qlt=80&fmt=webp" />
+              </div>
+            </li>
+            <li>
+              <div>
+                <img src="" />
+              </div>
+            </li>
+            <li>
+              <div>
+                <img src="" />
+              </div>
+            </li>
+            <li>
+              <div>
+                <img src="" />
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
