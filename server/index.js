c59dce2fe91a449c78e97317cf4d547cafc9d958
JoonJang95 <joon.jang95@gmail.com>
2019-01-05 01:44:48 -0500

    Add cors middleware

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const db = require('../db/index.js');
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
